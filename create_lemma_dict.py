07b7faceddc2fb0cb855ee03a14ecde3868866a9
John Stewart <cane.cubo@gmail.com>
2020-07-20 17:55:44 -0400

    added dictionary entries to inverted lemmata dictionary

--- create_lemma_dict.py
+++ create_lemma_dict.py
@@ -7,18 +7,31 @@ import importlib.machinery
 import pprint
 
 
-if __name__ == "__main__":
-	path = os.path.expanduser("forms_and_lemmas.py")
+def _load_file(file):
+	path = os.path.expanduser(file)
 	loader = importlib.machinery.SourceFileLoader("file", path)
 	module = loader.load_module()
 
+	return module
+
+if __name__ == "__main__":
+	mod = _load_file('forms_and_lemmas.py')
+
 	inverted_index = {}
-	for (lemma, forms) in module.forms_and_lemmas.items():
+	for (lemma, forms) in mod.forms_and_lemmas.items():
 		for form in forms:
 			lemmas = inverted_index.get(form, [])
 			if lemma not in lemmas:
 				lemmas.append(lemma)
 				inverted_index[form] = lemmas
+
+	mod = _load_file('entries.py')
+	for lemma, _ in mod.entries:
+		lemmas = inverted_index.get(lemma, [])
+		if lemma not in lemmas:
+			lemmas.append(lemma)
+			inverted_index[lemma] = lemmas
+
 
 	pp = pprint.PrettyPrinter()
 	print("inverted_index = ", end='')
